0e43b1128485a86c62b94ad074a92d58903c8333
Lila Bergamini <lila.bergamini@hotmail.com>
2023-01-03 13:48:43 -0300

    Autocomplete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,57 +3,70 @@ import s from "../styles/Home.module.css";
 import { NavBar } from ".";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import * as actions from "../redux/product/actions";
 
 export const Home = () => {
   const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+
   useEffect(() => {
     dispatch(actions.clearProduct());
-    console.log(localStorage.getItem('site'))
+    console.log(localStorage.getItem("site"));
   }, []);
+
+  useEffect(() => {
+    dispatch(actions.getProducts());
+  }, []);
+
   return (
     <div>
-      <div>
-        <NavBar />
-      </div>
-      <div className={s.homecontainer}>
-        <Link to={"/cards/entradas"}>
-          <div className={`${s.card} ${s.card__entradas}`}>
-            <h2>Entradas</h2>
+      {products.length != 0 ? (
+        <div>
+          <div>
+            <NavBar />
           </div>
-        </Link>
+          <div className={s.homecontainer}>
+            <Link to={"/cards/entradas"}>
+              <div className={`${s.card} ${s.card__entradas}`}>
+                <h2>Entradas</h2>
+              </div>
+            </Link>
 
-        <Link to={"/cards/ensaladas"}>
-          <div className={`${s.card} ${s.card__ensaladas}`}>
-            <h2>Ensaladas</h2>
-          </div>
-        </Link>
+            <Link to={"/cards/ensaladas"}>
+              <div className={`${s.card} ${s.card__ensaladas}`}>
+                <h2>Ensaladas</h2>
+              </div>
+            </Link>
 
-        <Link to={"/cards/sopas"}>
-          <div className={`${s.card} ${s.card__sopas}`}>
-            <h2>Sopas</h2>
-          </div>
-        </Link>
+            <Link to={"/cards/sopas"}>
+              <div className={`${s.card} ${s.card__sopas}`}>
+                <h2>Sopas</h2>
+              </div>
+            </Link>
 
-        <Link to={"/cards/principales"}>
-          <div className={`${s.card} ${s.card__principales}`}>
-            <h2>Platos principales</h2>
-          </div>
-        </Link>
+            <Link to={"/cards/principales"}>
+              <div className={`${s.card} ${s.card__principales}`}>
+                <h2>Platos principales</h2>
+              </div>
+            </Link>
 
-        <Link to={"/cards/bebidas"}>
-          <div className={`${s.card} ${s.card__bebidas}`}>
-            <h2>Bebidas</h2>
-          </div>
-        </Link>
+            <Link to={"/cards/bebidas"}>
+              <div className={`${s.card} ${s.card__bebidas}`}>
+                <h2>Bebidas</h2>
+              </div>
+            </Link>
 
-        <Link to={"/cards/postres"}>
-          <div className={`${s.card} ${s.card__postres}`}>
-            <h2>Postres</h2>
+            <Link to={"/cards/postres"}>
+              <div className={`${s.card} ${s.card__postres}`}>
+                <h2>Postres</h2>
+              </div>
+            </Link>
           </div>
-        </Link>
-      </div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,45 +1,125 @@
-// import React from "react";
-// import { useState } from "react";
-// import { useDispatch } from "react-redux";
-// import search from "../images/Search.svg";
-// import s from "../styles/SearchBar.module.css";
-// import * as actions from "../redux/product/actions";
-// import { useNavigate } from "react-router-dom";
+import React from "react";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import search from "../images/Search.svg";
+import { useSelector } from "react-redux";
+import s from "../styles/SearchBar.module.css";
+import * as actions from "../redux/product/actions";
+import { useNavigate } from "react-router-dom";
+import Autosuggest from "react-autosuggest";
+import { useEffect } from "react";
+import { all } from "axios";
 
-// export const SearchBar = () => {
-//   const [input, setInput] = useState("");
-//   const dispatch = useDispatch();
-//   const navegate = useNavigate();
+export const SearchBar = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { products } = useSelector((state) => state.products);
 
-//   const handleChange = (e) => {
-//     setInput(e.target.value);
-//   };
+  // useEffect(() => {
+  //   if (products.length == 0) {
+  //     dispatch(actions.getProducts());
+  //     console.log("PRODUCTS", products);
+  //   }
+  // }, [products]);
 
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     if (input !== "") {
-//       dispatch(actions.getProductByName(input));
-//       setInput("");
-//       navegate("/resultsearch");
-//     }
-//   };
-//   return (
-//     <div className={s.container_search}>
-//       <form className={s.container} onSubmit={handleSubmit}>
-//         <div className={s.containerInput}>
-//           <input
-//             className={s.input}
-//             type="text"
-//             name="search"
-//             placeholder="Buscar un plato"
-//             value={input}
-//             onChange={handleChange}
-//           />
-//         </div>
-//         <button className={s.btn} type="submit">
-//           <span className="material-symbols-outlined">search</span>
-//         </button>
-//       </form>
-//     </div>
-//   );
-// };
+  const allProducts = products.map((p) => {
+    return {
+      name: p.name,
+      category: p.category[0].name,
+    };
+  });
+
+  const [productsList, setProductsList] = useState(allProducts); // ----------> Estado que almacena todos los productos existentes
+  const [inputValue, setInputValue] = useState(""); // ----------> Estado que setea el valor del input para luego compararlo con las opciones de arriba
+  const [selectedProduct, setSelectedProduct] = useState({}); // ----------> Estado que almacena la opcion seleccionada
+  //---------------------- verificación y normalización de valor ingresado -----------------------------------------------------------
+  const filterProduct = (inputValue) => {
+    const clearedValue = inputValue.value.trim().toLowerCase(); // ----------> toma el valor ingresado y lo "limpia" eliminando espacios y mayusculas
+    let data;
+    let filteredProducts = productsList.filter((p) => {
+      data = p.name + "-" + p.category; //-----------> toma el estado de todas las opciones
+
+      if (
+        data
+          .toLocaleLowerCase()
+          .normalize("NFD")
+          .replace(/[\u0300-\u036f]/g, "")
+          .includes(clearedValue)
+      ) {
+        // console.log("DATA", data);
+        // console.log("PRODUCTO QUE COINCIDE", p);
+        return p;
+      }
+    });
+    return clearedValue.length === 0 ? [] : filteredProducts;
+  };
+
+  //---------------------- el estado product almacenará los valores que coindidan con lo ingresado -------------------------------------
+
+  const onSuggestionsFetchRequested = (inputValue) => {
+    setProductsList(filterProduct(inputValue));
+  };
+  //---------------------- el estado product se borrará por completo ---------------------------------------------------------------------
+  const onSuggestionsClearRequested = () => {
+    setProductsList([]);
+  };
+  //---------------------- el estado product almacenará la opción seleccionada -----------------------------------------------------------
+  const getSuggestionValue = (suggestion) => {
+    return `${suggestion.name}`;
+  };
+  //---------------------- metodo que selecciona la opcion y la almacena en el estado -----------------------------------------------------
+
+  const selectProduct = (suggestion) => {
+    console.log(suggestion.name);
+    setSelectedProduct(suggestion.name);
+  };
+  //---------------------- este método renderiza las sugerencias --------------------------------------------------------------------------
+
+  const renderSuggestion = (suggestion) => (
+    <div
+      onClick={() => {
+        selectProduct(suggestion);
+      }}
+    >
+      {`${suggestion.name} - ${suggestion.category}`}
+    </div>
+  );
+
+  const onChange = (e, { newValue }) => {
+    setInputValue(newValue);
+  };
+
+  const inputProps = {
+    placeholder: "Buscar un plato",
+    value: inputValue,
+    onChange,
+  };
+  //--------------------------------------------------------------------------------------------------------------------------------------------------
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (inputValue !== "") {
+      dispatch(actions.getProductByName(inputValue));
+      setInputValue("");
+      navigate("/resultsearch");
+    }
+  };
+  return (
+    <div className={s.container_search}>
+      <form className={s.container} onSubmit={handleSubmit}>
+        <div className={s.containerInput}>
+          <Autosuggest
+            suggestions={productsList}
+            onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+            onSuggestionsClearRequested={onSuggestionsClearRequested}
+            getSuggestionValue={getSuggestionValue}
+            renderSuggestion={renderSuggestion}
+            inputProps={inputProps}
+          />
+        </div>
+        <button className={s.btn} type="submit">
+          <span className="material-symbols-outlined">search</span>
+        </button>
+      </form>
+    </div>
+  );
+};
