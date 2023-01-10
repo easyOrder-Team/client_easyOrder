048ff85e3811f6b426b877665147658759042155
Lila Bergamini <lila.bergamini@hotmail.com>
2023-01-10 20:41:22 -0300

    reservation ruta

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ import ReviewDetail from "./components/ReviewDetails";
 import { Ventas } from "./components/Ventas";
 import { VentasDetail } from "./components/VentasDetail";
 import { useAuth0 } from "@auth0/auth0-react";
+import { CreateReservation } from "./components/CreateReservation";
 
 function App() {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -50,6 +51,7 @@ function App() {
         <Route path="/details/:id" element={<Details />} />
         <Route path="/createProfile" element={<CreateProfile />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/createReservation" element={<CreateReservation />} />
         <Route
           path="/pagepay"
           element={
@@ -75,7 +77,7 @@ function App() {
         <Route path="/orderDetail/:id" element={<OrderDetail />} />
         <Route path="/reviewDetail/:id" element={<ReviewDetail />} />
         <Route path="/ventas" element={<Ventas />} />
-        <Route path="/ventas/:id" element={<VentasDetail/>} />
+        <Route path="/ventas/:id" element={<VentasDetail />} />
         <Route path="/updateProduct/:id" element={<UpdateProduct />} />
       </Routes>
     </div>
