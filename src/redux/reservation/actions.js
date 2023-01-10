3bbc5b5b654b0c1b9c9e0f63a40bf1da7438517f
Lila Bergamini <lila.bergamini@hotmail.com>
2023-01-10 20:28:16 -0300

    reservation action

--- src/redux/reservation/actions.js
+++ src/redux/reservation/actions.js
@@ -2,28 +2,40 @@ import * as types from "../types";
 import axios from "axios";
 
 export const getReservationById = (id) => {
-    return (dispatch) =>
-      axios
-        .get(`${import.meta.env.VITE_URL}/api/v1/reservation/${id}`)
-        .then((response) => {
-          console.log(response.data)
-          dispatch({
-            type: types.GET_RESERVAS_ACT_BY_ID_PROFILE,
-            payload: response.data,
-          });
-        })
-        .catch((error) => console.log(error));
-  };
+  return (dispatch) =>
+    axios
+      .get(`${import.meta.env.VITE_URL}/api/v1/reservation/${id}`)
+      .then((response) => {
+        console.log(response.data);
+        dispatch({
+          type: types.GET_RESERVAS_ACT_BY_ID_PROFILE,
+          payload: response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+};
 
-  export const deleteReservation = (id) => {
-    return (dispatch) =>
-      axios
-        .delete(`${import.meta.env.VITE_URL}/api/v1/reservation/${id}`)
-        .then((response) => {
-          dispatch({
-            type: types.DELETE_RESERVATION,
-            payload: id,
-          });
-        })
-        .catch((error) => console.log(error));
-  };
+export const deleteReservation = (id) => {
+  return (dispatch) =>
+    axios
+      .delete(`${import.meta.env.VITE_URL}/api/v1/reservation/${id}`)
+      .then((response) => {
+        dispatch({
+          type: types.DELETE_RESERVATION,
+          payload: id,
+        });
+      })
+      .catch((error) => console.log(error));
+};
+export const createReservation = (reservation) => {
+  console.log(reservation);
+  return (dispatch) =>
+    axios
+      .post(`http://localhost:3000/api/v1/reservation/`, reservation)
+      .then((response) => {
+        dispatch({
+          type: types.CREATE_RESERVATION,
+        });
+      })
+      .catch((error) => console.log(error));
+};
