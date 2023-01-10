f989fdad7d6b036ff74882a46396d9e0b8859394
Lila Bergamini <lila.bergamini@hotmail.com>
2023-01-10 20:43:28 -0300

    site actions

---
+++ src/redux/site/actions.jsx
@@ -0,0 +1,16 @@
+import * as types from "../types";
+import axios from "axios";
+
+export const getSiteActivas = (id) => {
+  return (dispatch) =>
+    axios
+      .get(`http://localhost:3000/api/v1/site/avalible`)
+      .then((response) => {
+        console.log(response.data);
+        dispatch({
+          type: types.GET_SITE_ACTIVAS,
+          payload: response.data,
+        });
+      })
+      .catch((error) => console.log(error));
+};
