40246c5bd9a89e82cbc5003782c4e9d03a8e6006
Miguel Gallardo <miguel.gallardof@icloud.com>
2023-01-13 14:47:01 -0500

    styles update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -21,35 +21,41 @@ export const Card = ({ name, price, image, description, edit, borrar, id }) => {
     dispatch(actionProducts.getProducts());
   };
 
-  const handleEdit = (id) =>{
-    navigate(`/updateProduct/${id}`)
-  }
+  const handleEdit = (id) => {
+    navigate(`/updateProduct/${id}`);
+  };
 
   return (
     <div className={style.card__container}>
       <Link to={`/details/${id}`}>
-      <div>
-        <img className={style.card__img} src={image} alt="plato" />
-      </div>
+        <div>
+          <img className={style.card__img} src={image} alt="plato" />
+        </div>
       </Link>
 
       <div className={style.card_all_info}>
-        
-          <div className={style.card__info}>
-            <h4 className={style.card_description_A}>
-              <b>{name}</b>
-            </h4>
-            <br />
-            <div className={style.card_description_B}>
-              {description.slice(0, 20) + "..."}
-            </div>
+        <div className={style.card__info}>
+          <h4 className={style.card_description_A}>
+            <b>{name}</b>
+          </h4>
+          <br />
+          <div className={style.card_description_B}>
+            {description.slice(0, 20) + "..."}
           </div>
-      
+        </div>
+
         <div className={style.card_description_C}>
           <div>{formatoPesosMxn(price)}</div>
           <div className={style.card_flex}>
             <div>
-              {edit && <button className={style.edit_btn} onClick={()=>handleEdit(id)}>{edit}</button>}
+              {edit && (
+                <button
+                  className={style.edit_btn}
+                  onClick={() => handleEdit(id)}
+                >
+                  {edit}
+                </button>
+              )}
             </div>
             {!borrar ? (
               <span
